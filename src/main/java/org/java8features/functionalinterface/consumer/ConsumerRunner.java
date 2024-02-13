da2a1e6a51090dd0c11382ad7ae51bbb9d7185e6
agiratech-vivek <vivek.a@agiratech.com>
2024-02-13 14:49:15 +0530

    Corrected the sentence formatting

--- src/main/java/org/java8features/functionalinterface/consumer/ConsumerRunner.java
+++ src/main/java/org/java8features/functionalinterface/consumer/ConsumerRunner.java
@@ -8,11 +8,7 @@ import java.util.function.Consumer;
 public class ConsumerRunner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Consumer<List<Integer>> getSquare = (list) -> {
-            for(int index = 0; index < list.size(); index++){
-                list.set(index, list.get(index) * list.get(index));
-            }
-        };
+        Consumer<List<Integer>> getSquare = getSquareList();
         Consumer<List<Integer>> displayList = list -> list.forEach(num -> System.out.println(num));
 
         System.out.println("Please provide the number of elements you want to insert");
@@ -22,8 +18,25 @@ public class ConsumerRunner {
         while(range-- > 0){
             numberList.add(scanner.nextInt());
         }
-        System.out.println("Press 1: Modify List, Press 2: Display List");
-        int option = scanner.nextInt();
+        while(true){
+            System.out.println("Press 1: Modify List, Press 2: Display List");
+            int option = scanner.nextInt();
+            if (option == 1) {
+                getSquare.accept(numberList);
+            } else if (option == 2) {
+                displayList.accept(numberList);
+            } else{
+                break;
+            }
+        }
+    }
 
+    private static Consumer<List<Integer>> getSquareList() {
+        Consumer<List<Integer>> getSquare = (list) -> {
+            for(int index = 0; index < list.size(); index++){
+                list.set(index, list.get(index) * list.get(index));
+            }
+        };
+        return getSquare;
     }
 }
